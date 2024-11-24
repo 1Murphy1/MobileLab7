adfc7e1f778f07d0ec59396c46bdda8108e50349
1Murphy1 <115133958+1Murphy1@users.noreply.github.com>
2024-11-24 14:58:30 +0700

    task2

---
+++ MyDialer2Task2/app/src/main/java/com/example/mydialer/ContactAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.mydialer
+
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactAdapter : ListAdapter<Contact, ContactAdapter.ContactViewHolder>(ContactDiffCallback()){
+
+    class ContactViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textName: TextView = itemView.findViewById(R.id.textName)
+        val textPhone: TextView = itemView.findViewById(R.id.textPhone)
+        val textType: TextView = itemView.findViewById(R.id.textType)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.rview_item, parent, false)
+        return ContactViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+        val contact = getItem(position)
+        holder.textName.text = contact.name
+        holder.textPhone.text = contact.phone
+        holder.textType.text = contact.type
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL).apply {
+                data = Uri.parse("tel:${contact.phone}")
+            }
+            holder.itemView.context.startActivity(intent)
+        }
+    }
+}
